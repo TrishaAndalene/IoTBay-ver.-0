03a5638e6464a2bfd0c8ad17e4a18c6137aebd46
Mirror Wanderer <67412789+TrishaAndalene@users.noreply.github.com>
2025-03-23 16:13:36 +1100

    Prototype of sign out page

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// for changing the sign up page
+var currentPage;
+var pages = ["page-1", "page-2"];
+
+function continuePage(x){
+    currentPage = x;
+
+    for (var i = 0; i < pages.length; i++){
+        document.getElementById(pages[i]).style.display = "none";
+    }
+    console.log(pages[x-1]);
+    document.getElementById(pages[x-1]).style.display = "flex";
+}
