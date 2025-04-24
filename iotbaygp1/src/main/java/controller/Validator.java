9a94a784aef3512a823b7d884801b8a6441d4b2b
elle-christie <Ellen.D.Christie@student.uts.edu.au>
2025-04-24 11:46:40 +1000

    deleting controller files

--- iotbaygp1/src/main/java/controller/Validator.java
+++ iotbaygp1/src/main/java/controller/Validator.java
@@ -1,33 +1,35 @@
 package controller;
+import java.io.Serializable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Validator {
-    private final String nameValidation = "([A-Za-z]+)";
-    private final String passwordValidation = "([a-zA-Z0-9]+){8,}";
-    private final String emailValidation = "([a-zA-Z0-9]+)(([._-])([a-zA-Z0-9]+))*(@)([a-z]+)(.)([a-z]{2})((([.])[a-z]{0,2})*)";
+public class Validator implements Serializable{ 
+   
+    private String emailPattern = "([a-zA-Z0-9]+)(([._-])([a-zA-Z0-9]+))*(@)([a-z]+)(.)([a-z]{3})((([.])[a-z]{0,2})*)";      
+   private String namePattern = "([A-Z][a-z]+[\\s])+[A-Z][a-z]*";       
+   private String passwordPattern = "[a-z0-9]{4,}";       
+              
+   public Validator(){    }       
 
-    public Validator(){}
-
-    public boolean validate(String ptrn, String input){
-        Pattern pattern = Pattern.compile(ptrn);
-        Matcher match = pattern.matcher(input);
-        return match.matches();
-    }   
-
-    public boolean checkEmpty(String email, String password){
-        return email.isEmpty() || password.isEmpty();
-    }
-
-    public boolean validateEmail(String email){
-        return validate(this.emailValidation, email);
-    }
-
-    public boolean validatePassword(String password){
-        return validate(this.passwordValidation, password);
-    }
-
-    public boolean validateName(String name){
-        return validate(this.nameValidation, name);
-    }
+   public boolean validate(String pattern, String input){       
+      Pattern regEx = Pattern.compile(pattern);       
+      Matcher match = regEx.matcher(input);       
+      return match.matches(); 
+   }       
+ 
+   public boolean checkEmpty(String email, String password){       
+      return  email.isEmpty() || password.isEmpty();   
+   }
+   
+   public boolean validateEmail(String email){                       
+      return validate(emailPattern,email);   
+   }
+       
+   public boolean validateName(String name){
+      return validate(namePattern,name); 
+   }       
+   
+   public boolean validatePassword(String password){
+      return validate(passwordPattern,password); 
+   }          
 }
